e983be6a8c4c5bc52a6de70a98308c81673f0608
Igor Tomić <igor.tomic@mev.hr>
2018-06-04 21:58:35 +0200

    Revert "Connection string, DBContext"
    This reverts commit a9f7c47cad1002badda70496d44ac855490bceb9.

--- Eredar/Eredar/Models/Korisnici.cs
+++ Eredar/Eredar/Models/Korisnici.cs
@@ -1,46 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Eredar.Models
 {
-	[Table("eredarhr_db")]
     public class Korisnici
     {
-		[Key]
         public int Id { get; set; }
-		[Column("vrsta_korisnika")]
         public string Vrsta_Korisnika { get; set; }
-		[Column("ime")]
-		public string Ime { get; set; }
-		[Column("prezime")]
-		public string Prezime { get; set; }
-		[Column("email")]
-		[DataType(DataType.EmailAddress)]
-		public string Email { get; set; }
-		[Column("password")]
-		[DataType(DataType.Password)]
-		public string Password { get; set; }
-		[Column("id_mjesto_grad")]
-		public int Id_mjesto_grad { get; set; }
-		[Column("opis")]
-        public string Opis { get; set; }
-		[Column("aktivacijski_kljuc")]
-		public string Aktivacijski_kljuc { get; set; }
-		[Column("aktivan")]
-		public int Aktivan { get; set; }
-		[Column("email_send")]
-		public int Email_send { get; set; }
-		[Column("created_at")]
-		public DateTime Created_at { get; set; }
-		[Column("uploaded_at")]
-		public DateTime Updated_at { get; set; }
-		[Column("licenca")]
-		public DateTime Licenca { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Id_mjesto_grad { get; set; }
 
-		public ICollection<Korisnici> PopisKorisnika { get; set; }
+        public string Opis { get; set; }
+
+        public string Aktivacijski_kljuc { get; set; }
+
+        public int Aktivan { get; set; }
+        public int Email_send { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public DateTime Licenca { get; set; }
     }
 }
